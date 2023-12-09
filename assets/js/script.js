e830aa9689ce662585e9601586ca1bfb45659919
mikel072889 <145473019+mikel072889@users.noreply.github.com>
2023-12-09 00:13:31 +0000

    Create first constant variable in javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,182 @@
+const questions = [
+    {
+        question: "What part of a dog is as unique as a human fingerprint?",
+        options: ["Ears", "Tail", "Nose", "Stomach"],
+        correctAnswer: "Nose",
+        trivia: "Dog’s nose, when combined with the shape of its nostril openings, is believed to make a mark that is distinctive enough to conclusively identify one dog among all others."
+    },
+    {
+        question:"",
+        options:[],
+        correctAnswer:"",
+        trivia:""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+    {
+        question: "",
+        options: [],
+        correctAnswer: "",
+        trivia: ""
+    },
+];
